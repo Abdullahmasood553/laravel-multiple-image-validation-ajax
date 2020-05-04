9bbbccc7f9650c15ddadde545991700876506ffd
abdullah553 <abdullahmasood553@gmail.com>
2020-05-05 02:48:16 +0500

    added modification

--- storage/framework/views/c344211a5af616e20a5b280b4e40199dca7fbd28.php
+++ storage/framework/views/c344211a5af616e20a5b280b4e40199dca7fbd28.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MyApp | Abnation</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/login.css')); ?>">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
     
     <!-- Bootstrap core CSS -->
@@ -82,12 +82,12 @@
 		style="z-index:10000; display: none; background: green; font-weight: 450; width: 350px; position: fixed; top: 80%; left: 5%; color: white; padding: 5px 20px">
   </div>
 
-      @yield('content')
+      <?php echo $__env->yieldContent('content'); ?>
         <script src="http://code.jquery.com/jquery-3.4.1.js"></script>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.29.2/sweetalert2.all.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
         <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
-    @yield('javascript')
+    <?php echo $__env->yieldContent('javascript'); ?>
 </body>
-</html>
+</html><?php /**PATH C:\xampp\htdocs\image_validation\resources\views/layouts/master.blade.php ENDPATH**/ ?>
